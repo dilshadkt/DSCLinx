f437bd8b0f1c76dfdfc9b6a7394ee0632ae9e807
sajinprakas <sajinp297@gmail.com>
2024-05-09 10:59:30 +0530

    Project: improved text styling

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -22,7 +22,7 @@ export default function ProjectCard({ link, image, name }) {
             <div className="bg-[linear-gradient(4deg,_#00436399_19.14%,_rgb(13_38_35_/_0%)_86.54%)] absolute top-[0%] bottom-[0%] left-[0%] right-[0%]"></div>
             <div className="pb-[40px] pl-[40px] pr-[40px] absolute top-auto bottom-[0%] left-[0%] right-[0%] max-lg:pb-[20px] max-lg:pl-[20px] max-lg:pr-[20px]">
               <div className="flex items-center justify-between">
-                <h4 className="text-[28px] font-semibold text-[#fff] max-lg:mt-[10px] max-lg:mb-[15px] max-lg:text-[19px] max-md:text-[26px] max-sm:text-[17px]">
+                <h4 className="text-[28px] font-semibold text-[#fff] max-lg:mt-[10px] max-lg:mb-[15px] max-lg:text-[19px] max-md:text-[26px] max-sm:text-[17px] leading-[34px]">
                   {name}
                 </h4>
                 <p className="flex justify-center items-center w-[55px] h-[55px] border-[2px] border-[#fff] rounded-[100%] max-lg:w-[52px] max-lg:h-[52px] max-lg:top-[20px] max-lg:right-[20px] max-md:w-[40px] max-md:h-[40px]">
